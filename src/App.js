df3593f3fc87295723622485e962bbd9a8119d52
Konnov1 <mkonnov155@gmail.com>
2026-02-23 08:12:25 +0000

    1proekt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react'
 import './index.scss';
 
 function App() {
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1></h1>
         <button className="minus">- Минус</button>
         <button className="plus">Плюс +</button>
       </div>
